e27eac78749483dffc93ac23d17e81953ab56cf4
pareshpbhayani <pareshpbhayani@gmail.com>
2024-04-18 19:20:09 +0530

    Exeption handling

--- VideoAutoSplitter/Program.cs
+++ VideoAutoSplitter/Program.cs
@@ -13,5 +13,16 @@ namespace VideoAutoSplitter
             ApplicationConfiguration.Initialize();
             Application.Run(new FrmHome());
         }
+
+        private const string _messageBoxTitle = "Video Auto Splitter";
+        static void Application_ThreadException(object sender, ThreadExceptionEventArgs ex)
+        {
+            MessageBox.Show(ex.Exception.ToString(), _messageBoxTitle, MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+
+        static void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs ex)
+        {
+            MessageBox.Show((ex.ExceptionObject as Exception)?.ToString(), _messageBoxTitle, MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
     }
 }
